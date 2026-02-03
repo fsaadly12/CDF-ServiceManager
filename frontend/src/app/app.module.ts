d4fa746aa248cae03cfe6d69a1701b5948f1486b
fsaadly12 <saadlifadi9@gmail.com>
2026-02-03 20:15:10 +0100

    improve UI Bootstrap and Angular Material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material/material.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +14,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    RouterModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
 
   ],
   providers: [],
--- frontend/src/app/pages/dashboard/dashboard.module.ts
+++ frontend/src/app/pages/dashboard/dashboard.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
-
+import { MaterialModule } from '../../shared/material/material.module';
 @NgModule({
   declarations: [DashboardHomeComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    FormsModule
+    FormsModule,
+    MaterialModule
   ]
 })
 export class DashboardModule { }
